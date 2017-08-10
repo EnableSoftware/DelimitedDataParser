d546cc0abbb4f37555263b3634481e7ceca66aff
andrewenable <andrew.valentine@enable.com>
2017-08-10 12:36:58 +0100

    Update AssemblyInfo.cs

--- src/DelimitedDataParser/Properties/AssemblyInfo.cs
+++ src/DelimitedDataParser/Properties/AssemblyInfo.cs
@@ -5,6 +5,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Delimited Data Parser")]
 [assembly: AssemblyProduct("Delimited Data Parser")]
+[assembly: AssemblyDescription("C# library for parsing and exporting tabular data in delimited format (e.g. CSV).")]
+[assembly: AssemblyCompany("Enable · enable.com")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("DelimitedDataParser.Test")]
