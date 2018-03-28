dd73594dcde48198b2d827c82ac279a80c32bce8
Matt Brooks <matt.brooks@enable.com>
2018-03-28 12:09:21 +0100

    Add multi-target support #14

--- src/DelimitedDataParser/Properties/AssemblyInfo.cs
+++ src/DelimitedDataParser/Properties/AssemblyInfo.cs
@@ -3,12 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Delimited Data Parser")]
-[assembly: AssemblyProduct("Delimited Data Parser")]
-[assembly: AssemblyDescription("C# library for parsing and exporting tabular data in delimited format (e.g. CSV).")]
-[assembly: AssemblyCompany("Enable · enable.com")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("DelimitedDataParser.Test")]
-[assembly: AssemblyVersion("4.0.*")]
