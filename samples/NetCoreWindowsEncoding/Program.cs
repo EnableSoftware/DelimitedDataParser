43e4a64cbb0fd1e5fa7f2ef7833e8ee6a2236d81
Matt Brooks <matt.brooks@enable.com>
2018-05-14 11:36:02 +0100

    Add .NET Core sample app Demonstrates use of Windows-1252 encoding using .NET Core. Closes #20

---
+++ samples/NetCoreWindowsEncoding/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using DelimitedDataParser;
+
+namespace WindowsEncoding
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // When using DelimitedDataParser in a .NET Core app,
+            // be sure to include the NuGet package System.Text.Encoding.CodePages.
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            // Attempting to obtain the Windows-1252 encoding will fail on .NET Core
+            // if the required // code page is not correctly registered.
+            var windows1252 = Encoding.GetEncoding(1252);
+
+            // Try and ensure the console's encoding matches the character encoding
+            // of the file input data.
+            Console.OutputEncoding = windows1252;
+
+            var parser = new Parser
+            {
+                UseFirstRowAsColumnHeaders = false
+            };
+
+            using (var stream = new StreamReader("Windows1252.txt", windows1252))
+            using (var reader = parser.ParseReader(stream))
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0]);
+                }
+            }
+        }
+    }
+}
